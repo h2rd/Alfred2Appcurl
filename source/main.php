fe10d9c36289aa68ce86a93968e11677c5d2bb46
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2013-06-25 15:02:49 +0300

    add directory

---
+++ source/main.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/workflows.php';
+
+$wf = new Workflows();
+
+$query = $_SERVER['argv'][1];
+
+$detecturl = "http://www.appcurl.com/api/0/appcurl/qs/?q=" . urlencode( $query );
+$items = json_decode( $wf->request( $detecturl ), true);
+
+$wf->result(0, $query, 'Open in appcurl.com', '', 'icon.png');
+
+if ($items && $items['total']):
+    foreach($items['result'] as $item):
+        $url = "http://www.airomo.com/apps/" . $item['id'];
+
+        // TODO: Change icon.png to $item['icon'] when it will possible in Alfred
+        $wf->result($item['id'], $url, $item['title'], $url, 'icon.png');
+    endforeach;
+else:
+    $wf->result( 'appcurl', $query, 'No search suggestions found.', '', 'icon.png' );
+endif;
+
+echo $wf->toxml();
